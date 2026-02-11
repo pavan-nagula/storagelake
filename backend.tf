6911edaaf7fd68a42d72f8a9c0e53bab1238d7a6
Nagula Pavan <pavannagula2002@gmail.com>
2026-02-11 14:48:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "cats-pub-terraform-remote-backend-state"
     key                  = "terraform.tfstate"
-    region               = "us-west-2"
+    region               = "us-east-1"
     workspace_key_prefix = "terraform-cats-pub-hyderabad-ods/datalake/tfstates"
     dynamodb_table       = "cats-pub-terraform-remote-backend-436812234045"
     assume_role = {
